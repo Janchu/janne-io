2dcef0ad673a7f408057ca2411d082baac15ebf4
Janne Jäppinen <janne.jappinen@protonmail.com>
2023-04-23 20:37:30 +0300

    Add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <>
-      <main className="container mx-auto px-4">
+    <main className="container mx-auto px-4 min-h-screen flex flex-col justify-between gap-8">
+      <div>
         <Header />
         {children}
-      </main>
-    </>
+      </div>
+      <Footer />
+    </main>
   );
 }
