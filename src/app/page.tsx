c8f5b920abd7c4f16e0e4b3a2e13f1880a29e5d9
Janne Jäppinen <janne.jappinen@protonmail.com>
2026-02-17 15:32:05 +0200

    Run tailwind upgrade codemod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,21 +17,21 @@ const projects: ProjectItemType[] = [
     name: "OJK Discord Bot",
     description: "A discord bot developed for a group of friends.",
     url: "https://github.com/Janchu/ojk-discord-bot",
-    icon: <CodeBracketIcon className="h-6 w-6 flex-shrink-0" />,
+    icon: <CodeBracketIcon className="h-6 w-6 shrink-0" />,
     keywords: ["Discord.js", "Node.js"],
   },
   {
     name: "janne.io",
     description: "This portfolio website.",
     url: "https://github.com/Janchu/janne-io",
-    icon: <CodeBracketIcon className="h-6 w-6 flex-shrink-0" />,
+    icon: <CodeBracketIcon className="h-6 w-6 shrink-0" />,
     keywords: ["NextJS", "React", "Tailwind CSS", "TypeScript", "Vercel"],
   },
   {
     name: "Home server",
     description:
       "My personal home server that serves as a NAS and the hub for my home automation.",
-    icon: <ServerStackIcon className="h-6 w-6 flex-shrink-0" />,
+    icon: <ServerStackIcon className="h-6 w-6 shrink-0" />,
     keywords: ["Unraid", "NAS", "Docker", "Home Assistant"],
   },
 ];
@@ -96,12 +96,12 @@ export default function HomePage() {
 
               <div className="flex flex-col gap-4">
                 <div className="flex gap-2 items-center">
-                  <MapPinIcon className="h-6 w-6 flex-shrink-0" />
+                  <MapPinIcon className="h-6 w-6 shrink-0" />
                   <span>Vihti, Finland</span>
                 </div>
 
                 <div className="flex gap-2 items-center">
-                  <BuildingOfficeIcon className="h-6 w-6 flex-shrink-0" />
+                  <BuildingOfficeIcon className="h-6 w-6 shrink-0" />
                   <span>
                     Software Developer at{" "}
                     <a
@@ -114,7 +114,7 @@ export default function HomePage() {
                 </div>
 
                 <div className="flex gap-2 items-center">
-                  <CodeBracketIcon className="h-6 w-6 flex-shrink-0" />
+                  <CodeBracketIcon className="h-6 w-6 shrink-0" />
                   <a
                     className="font-medium underline hover:transition-colors text-black dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
                     href="https://github.com/Janchu"
@@ -124,7 +124,7 @@ export default function HomePage() {
                 </div>
 
                 <div className="flex gap-2 items-center">
-                  <LinkIcon className="h-6 w-6 flex-shrink-0" />
+                  <LinkIcon className="h-6 w-6 shrink-0" />
                   <a
                     className="font-medium underline hover:transition-colors text-black dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
                     href="https://www.linkedin.com/in/jannejäppinen/"
